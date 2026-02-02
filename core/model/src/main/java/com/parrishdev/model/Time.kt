073810f1a6cbf1cdaf9ed1c45700bfb2de8d0a2c
William Parrish <wrparrish@gmail.com>
2026-02-01 19:16:11 -0500

    Remove core:model module

--- core/model/src/main/java/com/parrishdev/model/Time.kt
+++
@@ -1,8 +0,0 @@
-package com.parrishdev.model
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class Time(@Json(name = "time")
-                val time: String = "")
