10cca84b1350bb6db3a58118a28e892fa6e989b2
William Parrish <wrparrish@gmail.com>
2026-02-01 19:16:12 -0500

    Remove features:results module

--- features/results/src/main/java/com/parrishdev/results/ResultViewState.kt
+++
@@ -1,7 +0,0 @@
-package com.parrishdev.results
-
-data class ResultViewState(
-    val isLoading: Boolean = true,
-    val results: List<RaceResult> = emptyList(),
-    val events: (ResultEvent) -> Unit = {}
-)
