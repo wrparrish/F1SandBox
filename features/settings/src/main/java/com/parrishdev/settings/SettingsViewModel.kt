997b3c42fe11a55511e6c5eb96b05dc9d2469bb6
William Parrish <wrparrish@gmail.com>
2026-02-01 19:16:12 -0500

    Remove features:settings module

--- features/settings/src/main/java/com/parrishdev/settings/SettingsViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.parrishdev.settings
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class SettingsViewModel @Inject constructor() : ViewModel()
