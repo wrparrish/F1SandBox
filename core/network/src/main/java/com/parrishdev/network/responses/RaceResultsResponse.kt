b74feb03fb3b386a1eb889e0607c220eea011511
William Parrish <wrparrish@gmail.com>
2026-02-01 19:16:11 -0500

    Remove core:network module

--- core/network/src/main/java/com/parrishdev/network/responses/RaceResultsResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.parrishdev.network.responses
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class RaceResultsResponse(@Json(name = "MRData")
-                               val data: MRData)
