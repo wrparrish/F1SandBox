dcd5b5d2e64e8cd546618e695968b6fa531eaa92
William Parrish <wrparrish@gmail.com>
2026-02-08 13:54:10 -0500

    remove responses and models for deprecated api

--- core/network/src/main/java/com/parrishdev/network/responses/RaceTableResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.parrishdev.network.responses
-
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class RaceTableResponse(
-    @Json(name = "MRData") val mrData: MRData // Reusing the existing MRData which contains RaceTable
-)
